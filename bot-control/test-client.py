f12bcc016b91f475ca17253863adda875802a81c
Marcel Gehre <gehre4@googlemail.com>
2017-06-06 21:50:17 +0200

    fixing

--- bot-control/test-client.py
+++ bot-control/test-client.py
@@ -3,14 +3,21 @@
 
 # Echo client program
 import socket
+import sys
 
 HOST = '127.0.0.1'    # The remote host
+if len(sys.argv) > 1:
+    print "Custom connection: " + sys.argv[1]
+    HOST = sys.argv[1]
 PORT = 13337              # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+
 
 try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
     while 1:
         s.sendall(raw_input())
 except KeyboardInterrupt:
+    s.close()
+except:
     s.close()
