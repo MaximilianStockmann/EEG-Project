47b4aa6042acdf20d7c2a304930d4d16d44866a9
stefreschke <stefan.reschke@outlook.de>
2017-06-08 23:02:31 +0200

    adding Resource-folder and CtBotConnection.cs (to it)

---
+++ GUI/WindowsFormsApplication1/WindowsFormsApplication1/Resources/CtBotConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// all Stuff for communicating with the CtBot
+// its all yours, Marcel ;)
+namespace WindowsFormsApplication1.Resources
+{
+    class CtBotConnection
+    {
+    }
+}
