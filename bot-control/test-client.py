3b4a4665c250646a6f8298ea015d21dd9affdfae
ZuBlomberg <gehre4@googlemail.com>
2017-06-08 17:02:52 +0200

    testing edit

--- bot-control/test-client.py
+++ bot-control/test-client.py
@@ -5,44 +5,55 @@
 import socket
 import sys
 
+manuel = False
+
 HOST = '127.0.0.1'    # The remote host
 if len(sys.argv) > 1:
     print "Custom connection: " + sys.argv[1]
     HOST = sys.argv[1]
+if len(sys.argv) > 2 and sys.argv[2] == "manuel":
+    manuel = True
 
 
 PORT = 13337              # The same port as used by the server
-
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
 
 
 def send(arg):
+    global s
     print arg
     s.sendall(arg)
 
-import pygame
-pygame.init()
+
+if manuel:
+    import pygame
+    pygame.init()
+    try:
+
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        send("forward")
+                    elif event.key == pygame.K_DOWN:
+                        send("backward")
+                    elif event.key == pygame.K_LEFT:
+                        send("left")
+                    elif event.key == pygame.K_RIGHT:
+                        send("right")
+
+                elif event.type == pygame.KEYUP:
+                    send("stop")
+    except KeyboardInterrupt:
+        s.close()
+        sys.exit(0)
+
+
+
+
 try:
-    while True:
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    send("forward")
-                elif event.key == pygame.K_DOWN:
-                    send("backward")
-                elif event.key == pygame.K_LEFT:
-                    send("left")
-                elif event.key == pygame.K_RIGHT:
-                    send("right")
-
-            elif event.type == pygame.KEYUP:
-                send("stop")
-except KeyboardInterrupt:
-    s.close()
-
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
     while 1:
         s.sendall(raw_input())
         print "Status: " + s.recv(20)
